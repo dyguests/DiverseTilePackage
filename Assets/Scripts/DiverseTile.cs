a27045c52ee563518e7e8e3c2ebaf96031bc7138
fanhl <dyguests@qq.com>
2022-07-30 19:27:55 +0800

    DiverseTileEditor

--- Assets/Scripts/DiverseTile.cs
+++ Assets/Scripts/DiverseTile.cs
@@ -6,6 +6,17 @@ namespace Koyou
     [CreateAssetMenu(fileName = "DiverseTile", menuName = "2D/Tiles/DiverseTile")]
     public class DiverseTile : RuleTile
     {
+        /// <summary>
+        /// The RuleTile to override
+        /// </summary>
+        public RuleTile m_Tile;
+
+        /// <summary>
+        /// Returns the Rule Tile for retrieving TileData
+        /// </summary>
+        [HideInInspector] public RuleTile m_InstanceTile;
+
+
         public MatchType matchType;
         public int matchMaskIn;
         public int matchMaskOut;

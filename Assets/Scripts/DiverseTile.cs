6d0db1a011fe7bc7959259e14db4829d4437104d
fanhl <dyguests@qq.com>
2022-07-30 19:19:24 +0800

    MatchType

--- Assets/Scripts/DiverseTile.cs
+++ Assets/Scripts/DiverseTile.cs
@@ -6,11 +6,20 @@ namespace Koyou
     [CreateAssetMenu(fileName = "DiverseTile", menuName = "2D/Tiles/DiverseTile")]
     public class DiverseTile : RuleTile
     {
-        
-        
+        public MatchType matchType;
+        public int matchMaskIn;
+        public int matchMaskOut;
+
         public override bool RuleMatch(int neighbor, TileBase other)
         {
             return base.RuleMatch(neighbor, other);
+        }
+
+        public enum MatchType
+        {
+            Normal,
+            MatchMask,
+            MatchMaskIO,
         }
     }
 }

a907cf4c82b0ddf2b03ce5f7d9c30213b3f8635b
fanhl <dyguests@qq.com>
2022-07-31 12:23:46 +0800

    add CrossTilemap.

---
+++ Assets/Plugins/Koyou/Tilemap/Scripts/CrossTilemap.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Koyou
+{
+    [RequireComponent(typeof(Tilemap))]
+    public class CrossTilemap : MonoBehaviour
+    {
+        [SerializeField] private CrossTilemap[] relatedTilemaps;
+
+        public CrossTilemap[] RelatedTilemaps => relatedTilemaps;
+
+        private Tilemap mTilemap;
+
+        public Tilemap Tilemap
+        {
+            get
+            {
+                if (mTilemap == null) mTilemap = GetComponent<Tilemap>();
+                return mTilemap;
+            }
+        }
+    }
+}
